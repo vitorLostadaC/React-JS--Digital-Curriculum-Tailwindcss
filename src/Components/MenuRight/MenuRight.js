d3de2f9440fd91bb7f3b5f158a3056a586efaa34
VitorCunhaP <vitorlostada@hotmail.com>
2022-01-22 20:44:32 -0300

    Technical abilities

--- src/Components/MenuRight/MenuRight.js
+++ src/Components/MenuRight/MenuRight.js
@@ -1,3 +1,47 @@
 export function MenuRight() {
-  return <div className="bg-menuRightColor ">...</div>;
+  return (
+    <div className="bg-menuRightColor p-6">
+      <section>
+        <h1 className="text-titleColor  text-xl font-semibold tracking-widest mb-4">
+          HABILIDADES TÉCINCAS
+        </h1>
+
+        <div className="flex flex-row gap-2 ">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
+            className="w-8"
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
+            className="w-8"
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            className="w-8"
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
